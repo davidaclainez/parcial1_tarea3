613e4776bc7fdedd53ec1ce4b9f6cd913b366c72
David Castillo <43655920+davidaclainez@users.noreply.github.com>
2022-06-26 15:30:05 -0600

    tarea3v1

---
+++ Ejercicio1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSumar_Clicked(object sender, EventArgs e)
+        {
+            var respuesta = Convert.ToInt32(firtNumber.Text) + Convert.ToInt32(secondNumber.Text);
+            await Navigation.PushAsync(new SecondPage(respuesta));
+        }
+
+        private async void btnRestar_Clicked(object sender, EventArgs e)
+        {
+            var respuesta = Convert.ToInt32(firtNumber.Text) - Convert.ToInt32(secondNumber.Text);
+            await Navigation.PushAsync(new SecondPage(respuesta));
+        }
+
+        private async void btnMultiplicar_Clicked(object sender, EventArgs e)
+        {
+            var respuesta = Convert.ToInt32(firtNumber.Text) * Convert.ToInt32(secondNumber.Text);
+            await Navigation.PushAsync(new SecondPage(respuesta));
+        }
+
+        private async void btnDividir_Clicked(object sender, EventArgs e)
+        {
+            var respuesta = Convert.ToInt32(firtNumber.Text) / Convert.ToInt32(secondNumber.Text);
+            await Navigation.PushAsync(new SecondPage(respuesta));
+        }
+    }
+}
